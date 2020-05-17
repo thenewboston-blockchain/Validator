e032606fa397cb04f558ab34d3edc62c75466bf2
Bucky <buckyroberts@gmail.com>
2020-05-17 10:49:48 -0700

    Sync

--- v1/registrations/models/bank_registration.py
+++ v1/registrations/models/bank_registration.py
@@ -1,13 +1,17 @@
 from django.db import models
 
 from v1.banks.models.bank import Bank
+from v1.network.constants.models import PROTOCOL_CHOICES
 from v1.network.models.network_registration import NetworkRegistration
+from v1.network.utils.format import format_node_address
 
 
 class BankRegistration(NetworkRegistration):
     bank = models.ForeignKey(Bank, on_delete=models.CASCADE, blank=True, null=True)
     identifier = models.CharField(max_length=256)
     ip_address = models.GenericIPAddressField()
+    port = models.PositiveSmallIntegerField(blank=True, null=True)
+    protocol = models.CharField(choices=PROTOCOL_CHOICES, max_length=5)
 
     class Meta:
         default_related_name = 'bank_registrations'
@@ -16,6 +20,6 @@ class BankRegistration(NetworkRegistration):
         return (
             f'ID: {self.id} | '
             f'Identifier: {self.identifier} | '
-            f'IP address: {self.ip_address} | '
+            f'IP address: {format_node_address(self)} | '
             f'Status: {self.status}'
         )
