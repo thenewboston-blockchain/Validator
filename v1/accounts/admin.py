a13933960a725d2011d6773c6d23001540630401
Bucky <buckyroberts@gmail.com>
2020-05-24 10:58:09 -0700

    Removing transaction model

--- v1/accounts/admin.py
+++ v1/accounts/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
 from .models.account import Account
-from .models.transaction import Transaction
 
 admin.site.register(Account)
-admin.site.register(Transaction)
