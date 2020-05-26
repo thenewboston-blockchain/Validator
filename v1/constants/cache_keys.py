cf3985982a9935b3f541eeff0408b93a31bea4d4
Bucky <buckyroberts@gmail.com>
2020-05-25 19:39:49 -0700

    More on caching system

--- v1/constants/cache_keys.py
+++ v1/constants/cache_keys.py
@@ -1,1 +1,10 @@
 BANK_BLOCK_QUEUE = 'BANK_BLOCK_QUEUE'
+BLOCK_CHAIN_HEAD_HASH = 'BLOCK_CHAIN_HEAD_HASH'
+
+
+def block_cache_key(block_hash):
+    """
+    Return cache key for (confirmed) blocks
+    """
+
+    return f'BLOCK_{block_hash}'
--- v1/tasks/blocks.py
+++ v1/tasks/blocks.py
@@ -1,12 +1,22 @@
+from hashlib import sha3_256 as sha3
+
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.core.cache import cache
 from thenewboston.blocks.signatures import verify_signature
 from thenewboston.utils.tools import sort_and_encode
 
-from v1.constants.cache_keys import BANK_BLOCK_QUEUE
+from v1.constants.cache_keys import BANK_BLOCK_QUEUE, BLOCK_CHAIN_HEAD_HASH, block_cache_key
 
 logger = get_task_logger(__name__)
+
+
+def get_block_hash_value(*, block):
+    """
+    Generate a balance lock from a Tx
+    """
+
+    return sha3(sort_and_encode(block)).digest().hex()
 
 
 @shared_task
@@ -16,6 +26,7 @@ def process_bank_block_queue():
     """
 
     bank_block_queue = cache.get(BANK_BLOCK_QUEUE)
+    block_chain_head_hash = cache.get(BLOCK_CHAIN_HEAD_HASH)
 
     for bank_block in bank_block_queue:
         block = bank_block.get('block')
@@ -25,4 +36,15 @@ def process_bank_block_queue():
             signature=bank_block.get('signature'),
             message=message
         )
-        logger.info(block)
+        confirmed_block = {
+            **block,
+            'block_identifier': block_chain_head_hash
+        }
+        block_hash_value = get_block_hash_value(block=confirmed_block)
+        cache.set(BLOCK_CHAIN_HEAD_HASH, block_hash_value, None)
+        cache.set(block_cache_key(block_hash_value), confirmed_block, None)
+
+        # TODO: Send this out to the original bank and all backup validators
+        logger.warning(confirmed_block)
+
+    cache.set(BANK_BLOCK_QUEUE, [], None)
