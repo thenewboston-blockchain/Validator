25d252324e843a8aa2afa3ed30d32ef73a4e00fc
Bucky <buckyroberts@gmail.com>
2020-05-27 11:51:30 -0700

    Commenting

--- v1/blocks/views/bank_block.py
+++ v1/blocks/views/bank_block.py
@@ -18,7 +18,7 @@ class BankBlockView(APIView):
 
         # TODO: Serializer will check everything except point balances (registered bank, block formatting, etc...)
         # TODO: Also throw an error if this validator is not a primary validator
-        # TODO: If everything is good, add it to the bank block queue
+        # TODO: If everything is good, add the entire bank block to the bank block queue
 
         queue = cache.get(BANK_BLOCK_QUEUE)
 
