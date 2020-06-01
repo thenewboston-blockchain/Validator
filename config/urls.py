656e9460e9c69ded9487d8fb0dfdd28be8a33443
Bucky <buckyroberts@gmail.com>
2020-05-31 19:23:32 -0700

    Static

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -21,3 +23,5 @@ urlpatterns = [
     path('', include('v1.validators.urls')),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
