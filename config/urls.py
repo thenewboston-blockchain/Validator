9d7042783c524baa0c27fff2c8d292ad3816b6de
Justin Kim <justinkim943@gmail.com>
2020-06-01 18:46:01 -0700

    install django debug toolbar

--- config/urls.py
+++ config/urls.py
@@ -25,3 +25,10 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
