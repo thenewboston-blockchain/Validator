b025400cfaaeedaefce92b76c338b7dcc76df985
Bucky <buckyroberts@gmail.com>
2020-06-13 07:40:52 -0700

    GET /head_block_hash

--- v1/self_configurations/urls.py
+++ v1/self_configurations/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .views.head_block_hash import HeadBlockHashDetail
 from .views.self_configuration import SelfConfigurationDetail
 from .views.self_transaction_fee_tier import SelfTransactionFeeTierView
 
@@ -7,6 +8,9 @@ urlpatterns = [
 
     # Self configuration
     path('config', SelfConfigurationDetail.as_view()),
+
+    # Head block hash
+    path('head_block_hash', HeadBlockHashDetail.as_view()),
 
     # Self transaction fee tiers
     path('self_transaction_fee_tiers', SelfTransactionFeeTierView.as_view()),
