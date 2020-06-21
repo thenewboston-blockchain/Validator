7142c72657ca67507046816350e320400fa85366
Bucky <buckyroberts@gmail.com>
2020-06-21 16:48:33 -0700

    Writing tests

--- v1/tasks/tests/test_is_block_valid.py
+++ v1/tasks/tests/test_is_block_valid.py
@@ -130,6 +130,36 @@ class TestIsBlockValid(TestBase):
             is_valid, account_balance = is_block_valid(block=block)
             self.assertEqual(is_valid, expected_result)
 
+    def test_invalid_amount(self):
+        """
+        Treasury balance = 10,000
+        Total amount = 10,005
+        """
+
+        block = {
+            'account_number': '0cdd4ba04456ca169baca3d66eace869520c62fe84421329086e03d91a68acdb',
+            'message': {
+                'balance_key': '0cdd4ba04456ca169baca3d66eace869520c62fe84421329086e03d91a68acdb',
+                'txs': [
+                    {
+                        'amount': 10000,
+                        'recipient': '484b3176c63d5f37d808404af1a12c4b9649cd6f6769f35bdf5a816133623fbc'
+                    },
+                    {
+                        'amount': 1,
+                        'recipient': '5e12967707909e62b2bb2036c209085a784fabbc3deccefee70052b6181c8ed8'
+                    },
+                    {
+                        'amount': 4,
+                        'recipient': 'ad1f8845c6a1abb6011a2a434a079a087c460657aad54329a84b406dce8bf314'
+                    }
+                ]
+            },
+            'signature': '85a255d720bf5897d65bad7ad410bab672d34bd0871ba20c1cb910a084d298e194fb86559eee093ab5d8c83dc7a785a235a3febb5d78f256066a8b70a271110a'
+        }
+        is_valid, account_balance = is_block_valid(block=block)
+        self.assertFalse(is_valid)
+
     def test_missing_transaction(self):
         """
         Block with missing transaction should be invalid
