0bd6f333df75aa9ed87156b0f4f0fa3f00bf4977
Bucky Roberts <buckyroberts@gmail.com>
2020-06-26 15:08:02 -0700

    Confirmation block chain segments with starting point param

--- v1/confirmation_blocks/urls.py
+++ v1/confirmation_blocks/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from .views.confirmation_block import ConfirmationBlockView
+from .views.confirmation_block_chain_segment import ConfirmationBlockChainSegmentView
 
 urlpatterns = [
 
     # Confirmation blocks
     path('confirmation_blocks', ConfirmationBlockView.as_view()),
+
+    # Confirmation block chain segment
+    path('confirmation_block_chain_segment/<str:block_identifier>', ConfirmationBlockChainSegmentView.as_view()),
 
 ]
