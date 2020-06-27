b434834a6c6db33cd4491fee376c9a6341645c05
Bucky <buckyroberts@gmail.com>
2020-06-27 13:06:15 -0700

    Working on confirmation validator initial sync

--- v1/self_configurations/management/commands/initialize_confirmation_validator.py
+++ v1/self_configurations/management/commands/initialize_confirmation_validator.py
@@ -1,10 +1,10 @@
 from django.db.models import Q
 from thenewboston.base_classes.connect_to_primary_validator import ConnectToPrimaryValidator
 from thenewboston.utils.fields import standard_field_names
-from thenewboston.utils.format import format_address
 from thenewboston.utils.network import fetch
 
 from v1.self_configurations.models.self_configuration import SelfConfiguration
+from v1.tasks.blocks import get_message_hash
 from v1.validators.models.validator import Validator
 
 """
@@ -20,7 +20,17 @@ Running this script will:
 class Command(ConnectToPrimaryValidator):
     help = 'Connect to primary validator'
 
-    def get_confirmation_block_chain_segment(self, block_identifier):
+    def get_confirmation_block(self, *, block_identifier):
+        """
+        Return confirmation block chain segment
+        """
+
+        address = self.get_primary_validator_address()
+        url = f'{address}/confirmation_blocks/{block_identifier}'
+        results = fetch(url=url, headers={})
+        return results
+
+    def get_confirmation_block_chain_segment(self, *, block_identifier):
         """
         Return confirmation block chain segment
         """
@@ -30,18 +40,22 @@ class Command(ConnectToPrimaryValidator):
         results = fetch(url=url, headers={})
         return results
 
-    @staticmethod
-    def get_initial_block_identifier(validator_config):
+    def get_initial_block_identifier(self, validator_config):
         """
         Return initial block identifier
 
         If seed_block_identifier, fetch related (seed) block and hash to get initial block identifier
+        Otherwise, return root_account_file_hash
         """
 
         seed_block_identifier = validator_config.get('seed_block_identifier')
 
         if not seed_block_identifier:
             return validator_config.get('root_account_file_hash')
+
+        confirmation_block = self.get_confirmation_block(block_identifier=seed_block_identifier)
+
+        return get_message_hash(message=confirmation_block['message'])
 
     def set_primary_validator(self, validator_config):
         """
@@ -63,3 +77,6 @@ class Command(ConnectToPrimaryValidator):
         self_configuration = SelfConfiguration.objects.first()
         self_configuration.primary_validator = validator
         self_configuration.save()
+
+        initial_block_identifier = self.get_initial_block_identifier(validator_config=validator_config)
+        print(initial_block_identifier)
