5b6f42e097b6a2f27c6678e47945c9a103fa57a4
Bucky <buckyroberts@gmail.com>
2020-06-27 13:35:21 -0700

    Pulling confirmation blocks from PV

--- v1/self_configurations/management/commands/initialize_confirmation_validator.py
+++ v1/self_configurations/management/commands/initialize_confirmation_validator.py
@@ -1,3 +1,5 @@
+from json import JSONDecodeError
+
 from django.db.models import Q
 from thenewboston.base_classes.connect_to_primary_validator import ConnectToPrimaryValidator
 from thenewboston.utils.fields import standard_field_names
@@ -30,6 +32,14 @@ class Command(ConnectToPrimaryValidator):
         results = fetch(url=url, headers={})
         return results
 
+    @staticmethod
+    def get_confirmation_block_from_results(*, block_identifier, results):
+        """
+        Return the confirmation block from results list
+        """
+
+        return next((i for i in results if i['message']['block_identifier'] == block_identifier), None)
+
     def get_confirmation_block_chain_segment(self, *, block_identifier):
         """
         Return confirmation block chain segment
@@ -37,10 +47,16 @@ class Command(ConnectToPrimaryValidator):
 
         address = self.get_primary_validator_address()
         url = f'{address}/confirmation_block_chain_segment/{block_identifier}'
-        results = fetch(url=url, headers={})
-        return results
 
-    def get_initial_block_identifier(self, validator_config):
+        try:
+            results = fetch(url=url, headers={})
+            return results
+        except JSONDecodeError:
+            return []
+        except Exception as e:
+            print(e)
+
+    def get_initial_block_identifier(self, primary_validator_config):
         """
         Return initial block identifier
 
@@ -48,22 +64,22 @@ class Command(ConnectToPrimaryValidator):
         Otherwise, return root_account_file_hash
         """
 
-        seed_block_identifier = validator_config.get('seed_block_identifier')
+        seed_block_identifier = primary_validator_config.get('seed_block_identifier')
 
         if not seed_block_identifier:
-            return validator_config.get('root_account_file_hash')
+            return primary_validator_config.get('root_account_file_hash')
 
         confirmation_block = self.get_confirmation_block(block_identifier=seed_block_identifier)
 
         return get_message_hash(message=confirmation_block['message'])
 
-    def set_primary_validator(self, validator_config):
+    def set_primary_validator(self, primary_validator_config):
         """
         Set primary validator
         """
 
         validator_field_names = standard_field_names(Validator)
-        validator_data = {k: v for k, v in validator_config.items() if k in validator_field_names}
+        validator_data = {k: v for k, v in primary_validator_config.items() if k in validator_field_names}
 
         Validator.objects.filter(
             Q(ip_address=validator_data.get('ip_address')) |
@@ -78,5 +94,27 @@ class Command(ConnectToPrimaryValidator):
         self_configuration.primary_validator = validator
         self_configuration.save()
 
-        initial_block_identifier = self.get_initial_block_identifier(validator_config=validator_config)
-        print(initial_block_identifier)
+        self.sync(primary_validator_config=primary_validator_config)
+
+    def sync(self, *, primary_validator_config):
+        """
+        Sync with primary validator
+        """
+
+        block_identifier = self.get_initial_block_identifier(primary_validator_config=primary_validator_config)
+        results = self.get_confirmation_block_chain_segment(block_identifier=block_identifier)
+
+        while results:
+            confirmation_block = self.get_confirmation_block_from_results(
+                block_identifier=block_identifier,
+                results=results
+            )
+
+            while confirmation_block:
+                block_identifier = get_message_hash(message=confirmation_block['message'])
+                confirmation_block = self.get_confirmation_block_from_results(
+                    block_identifier=block_identifier,
+                    results=results
+                )
+
+            results = self.get_confirmation_block_chain_segment(block_identifier=block_identifier)
