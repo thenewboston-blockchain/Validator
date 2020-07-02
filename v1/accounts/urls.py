8ca815937ab010ca9504235760096fe5e6144268
Bucky Roberts <buckyroberts@gmail.com>
2020-07-01 20:08:59 -0700

    GET /account_balance_lock/<str:account_number>

--- v1/accounts/urls.py
+++ v1/accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from .views.account import AccountView
+from .views.account_balance_lock import account_balance_lock_view
 
 urlpatterns = [
 
     # Accounts
     path('accounts', AccountView.as_view()),
+
+    # Account balance lock
+    path('account_balance_lock/<str:account_number>', account_balance_lock_view),
 
 ]
