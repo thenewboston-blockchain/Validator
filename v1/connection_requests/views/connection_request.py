93f64a9799c01f535c57ff76342fbd0c417f745a
Bucky <buckyroberts@gmail.com>
2020-07-05 16:28:25 -0700

    Return empty dict for JSON logic

--- v1/connection_requests/views/connection_request.py
+++ v1/connection_requests/views/connection_request.py
@@ -22,5 +22,5 @@ class ConnectionRequestView(APIView):
         )
         if serializer.is_valid():
             serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
+            return Response({}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
