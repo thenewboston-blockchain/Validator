47d5e3b8377d7e287b73d6f5dc2f150f198a642c
Bucky <buckyroberts@gmail.com>
2020-07-05 14:17:09 -0700

    NID check

--- v1/connection_requests/serializers/connection_request.py
+++ v1/connection_requests/serializers/connection_request.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+from thenewboston.constants.network import VERIFY_KEY_LENGTH
 from thenewboston.utils.fields import all_field_names
 
 from ..models.connection_request import ConnectionRequest
@@ -10,3 +12,36 @@ class ConnectionRequestSerializer(serializers.ModelSerializer):
         fields = '__all__'
         model = ConnectionRequest
         read_only_fields = all_field_names(ConnectionRequest)
+
+
+class ConnectionRequestSerializerCreate(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = ConnectionRequest
+        validators = [
+            UniqueTogetherValidator(
+                queryset=ConnectionRequest.objects.all(),
+                fields=['ip_address', 'port']
+            )
+        ]
+
+    def create(self, validated_data):
+        """
+        Create connection request and add to queue
+        """
+
+        connection_request = ConnectionRequest.objects.create(**validated_data)
+
+        return connection_request
+
+    @staticmethod
+    def validate_node_identifier(node_identifier):
+        """
+        Validate node_identifier length
+        """
+
+        if len(node_identifier) != VERIFY_KEY_LENGTH:
+            raise serializers.ValidationError(f'node_identifier must be {VERIFY_KEY_LENGTH} characters long')
+
+        return node_identifier
--- v1/connection_requests/views/connection_request.py
+++ v1/connection_requests/views/connection_request.py
@@ -1,8 +1,9 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ..models.connection_request import ConnectionRequest
-from ..serializers.connection_request import ConnectionRequestSerializer
+from ..serializers.connection_request import ConnectionRequestSerializer, ConnectionRequestSerializerCreate
 
 
 # connection_requests
@@ -16,3 +17,15 @@ class ConnectionRequestView(APIView):
 
         connection_requests = ConnectionRequest.objects.all()
         return Response(ConnectionRequestSerializer(connection_requests, many=True).data)
+
+    @staticmethod
+    def post(request):
+        """
+        description: Create connection request
+        """
+
+        serializer = ConnectionRequestSerializerCreate(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            connection_request = serializer.save()
+            return Response(ConnectionRequestSerializer(connection_request).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
