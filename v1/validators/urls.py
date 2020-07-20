106faf95dafa33466138d043bb417d2aab28043b
Martin Nowosad <mrtnnwsd@gmail.com>
2020-07-20 23:54:51 +0200

    added PrimaryValidatorView and extended validators.urls

--- v1/validators/urls.py
+++ v1/validators/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .views.validator import ValidatorView
+from .views.primary_validator import PrimaryValidatorView
 
 urlpatterns = [
 
     # Validators
     path('validators', ValidatorView.as_view()),
-
+    path('primary_validator_upgrade_request', PrimaryValidatorView.as_view())
 ]
---
+++ v1/validators/views/primary_validator.py
@@ -0,0 +1,7 @@
+from rest_framework.views import APIView
+
+
+class PrimaryValidatorView(APIView):
+
+    def post(self, request):
+        pass
