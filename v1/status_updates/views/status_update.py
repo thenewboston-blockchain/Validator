cd6111a874423f5c841f2683a95955371d1396e4
Martin Nowosad <mrtnnwsd@gmail.com>
2020-07-23 21:38:14 +0200

    moved todo flag

--- v1/status_updates/views/status_update.py
+++ v1/status_updates/views/status_update.py
@@ -13,11 +13,11 @@ class StatusUpdateView(APIView):
         bank_node_identifier = node_data['node_identifier']
         is_trusted = is_most_trusted_bank(node_identifier=bank_node_identifier)
 
-        # ----------
-        # TODO upgrade as primary validator
-        # ----------
-
         if is_trusted:
+            # ----------
+            # TODO upgrade as primary validator
+            # ----------
+
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
