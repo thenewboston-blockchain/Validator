09abfb2954b25e8612ca4c665f0aedc35ec6a021
Bucky <buckyroberts@gmail.com>
2020-07-23 19:32:37 -0700

    Test

--- v1/self_configurations/management/commands/initialize_confirmation_validator.py
+++ v1/self_configurations/management/commands/initialize_confirmation_validator.py
@@ -186,4 +186,5 @@ class Command(ConnectToPrimaryValidator):
 
             results = self.get_confirmation_block_chain_segment(block_identifier=block_identifier)
 
-        process_confirmation_block_queue.delay()
+        # TODO: Add back in delay
+        process_confirmation_block_queue()
