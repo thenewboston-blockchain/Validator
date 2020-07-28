7d5ead0caa1f8dc6de4c94676922347e6295cf2a
Rajat Sharma <47222723+rajat4665@users.noreply.github.com>
2020-07-29 00:45:51 +0530

    send_signal_patch_rquest removed (#31)
    Co-authored-by: grubox_rajat <rajat.sharma@grubox.in>

--- v1/tasks/signed_requests.py
+++ v1/tasks/signed_requests.py
@@ -2,32 +2,12 @@ import logging
 
 from celery import shared_task
 from thenewboston.utils.format import format_address
-from thenewboston.utils.network import patch, post
+from thenewboston.utils.network import post
 from thenewboston.utils.signed_requests import generate_signed_request
 
 from v1.self_configurations.helpers.signing_key import get_signing_key
 
 logger = logging.getLogger('thenewboston')
-
-
-@shared_task
-def send_signed_patch_request(*, data, ip_address, port, protocol, url_path):
-    """
-    Sign data and send to recipient
-    """
-
-    signed_request = generate_signed_request(
-        data=data,
-        nid_signing_key=get_signing_key()
-    )
-
-    node_address = format_address(ip_address=ip_address, port=port, protocol=protocol)
-    url = f'{node_address}{url_path}'
-
-    try:
-        patch(url=url, body=signed_request)
-    except Exception as e:
-        logger.exception(e)
 
 
 @shared_task
