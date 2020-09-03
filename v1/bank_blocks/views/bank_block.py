02c92e849600e1907b87f6eeaa9f65543eb311ad
Bucky Roberts <buckyroberts@gmail.com>
2020-09-03 16:38:37 -0700

    Improved block logic (#45)

--- v1/bank_blocks/views/bank_block.py
+++ v1/bank_blocks/views/bank_block.py
@@ -2,28 +2,31 @@ from django.core.cache import cache
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from thenewboston.constants.network import PRIMARY_VALIDATOR
+from thenewboston.serializers.network_block import NetworkBlockSerializer
 
 from v1.cache_tools.cache_keys import BLOCK_QUEUE
 from v1.decorators.nodes import is_signed_bank_block
+from v1.self_configurations.helpers.self_configuration import get_self_configuration
 from v1.tasks.block_queue import process_block_queue
+
+"""
+Banks will request a new primary validator if an error status (status code >= 400) is returned
+This view should check the block formatting only and not the validity of the transaction
+"""
 
 
 # bank_blocks
 class BankBlockView(APIView):
 
     @staticmethod
-    @is_signed_bank_block
-    def post(request):
+    def add_block_to_queue(block):
         """
-        description: Add a bank block to the queue
+        Add block to the queue
         """
 
-        # TODO: Serializer will check block formatting
-        # TODO: These initial checks should not hit the SQL database
-        # TODO: Also throw an error if this validator is not a primary validator
-        # TODO: If everything is good, add the block to the block queue
+        # TODO: Improved caching system
 
-        block = request.data.get('block')
         queue = cache.get(BLOCK_QUEUE)
 
         if queue:
@@ -34,4 +37,27 @@ class BankBlockView(APIView):
         cache.set(BLOCK_QUEUE, queue, None)
         process_block_queue.delay()
 
-        return Response({}, status=status.HTTP_200_OK)
+    @staticmethod
+    @is_signed_bank_block
+    def post(request):
+        """
+        description: Add a block to the queue
+        """
+
+        self_configuration = get_self_configuration(exception_class=RuntimeError)
+
+        if self_configuration.node_type != PRIMARY_VALIDATOR:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        block = request.data.get('block')
+
+        serializer = NetworkBlockSerializer(
+            data=block,
+            context={'request': request}
+        )
+
+        if serializer.is_valid():
+            BankBlockView.add_block_to_queue(block)
+            return Response({}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
