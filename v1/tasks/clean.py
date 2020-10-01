d5d3818291ca915fa9fa11aec2fb2659916d961a
Mikhail <mgponomarev@gmail.com>
2020-10-02 02:23:27 +0300

    validators clean up in progress

---
+++ v1/tasks/clean.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+from django.core.cache import cache
+from django.utils import timezone
+
+from v1.cache_tools.cache_keys import CLEAN_LAST_COMPLETED, CLEAN_STATUS
+from v1.clean.constants import CLEAN_STATUS_NOT_CLEANING
+
+
+@shared_task
+def start_clean():
+    """
+    Start a network clean
+    """
+
+    cache.set(CLEAN_LAST_COMPLETED, str(timezone.now()), None)
+    cache.set(CLEAN_STATUS, CLEAN_STATUS_NOT_CLEANING, None)
