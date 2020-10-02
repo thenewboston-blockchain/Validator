85afa58e6f07b04646a271293ca24e93932a874b
Mikhail <mgponomarev@gmail.com>
2020-10-02 03:35:57 +0300

    send websockets notifications on clean status changed

--- config/routing.py
+++ config/routing.py
@@ -1,9 +1,11 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+from v1.lean import routing as clean_routing
 from v1.crawl import routing as crawl_routing
 
 application = ProtocolTypeRouter({
     'websocket': URLRouter(
+        clean_routing.websocket_urlpatterns +
         crawl_routing.websocket_urlpatterns
     )
 })
--- v1/tasks/clean.py
+++ v1/tasks/clean.py
@@ -3,6 +3,7 @@ from django.core.cache import cache
 from django.utils import timezone
 
 from v1.cache_tools.cache_keys import CLEAN_LAST_COMPLETED, CLEAN_STATUS
+from v1.notifications.clean_status import send_clean_status_notification
 from v1.clean.constants import CLEAN_STATUS_NOT_CLEANING
 
 
@@ -14,3 +15,5 @@ def start_clean():
 
     cache.set(CLEAN_LAST_COMPLETED, str(timezone.now()), None)
     cache.set(CLEAN_STATUS, CLEAN_STATUS_NOT_CLEANING, None)
+
+    send_clean_status_notification()
