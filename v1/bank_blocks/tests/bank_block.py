96f9308a09c79f830670f729ad7b224b0e2ecf80
Duc Pham <phamcongduc1994@gmail.com>
2020-12-11 22:35:43 +0700

    Response for POST request with code 200 should include requested data

--- v1/bank_blocks/tests/bank_block.py
+++ v1/bank_blocks/tests/bank_block.py
@@ -12,4 +12,5 @@ def test_bank_block_post(client, primary_validator_configuration, signed_block, 
         expected=HTTP_200_OK,
     )
 
+    assert response == signed_block['block']
     assert cache.get(BLOCK_QUEUE) == [signed_block['block']]
