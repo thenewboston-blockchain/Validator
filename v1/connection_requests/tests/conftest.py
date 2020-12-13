29ef5c63648bf936d022b69940a49964c5029f1e
Duc Pham <phamcongduc1994@gmail.com>
2020-12-13 11:57:17 +0700

    Add 3 test cases for bank connection requests

--- v1/connection_requests/tests/conftest.py
+++ v1/connection_requests/tests/conftest.py
@@ -1,4 +1,52 @@
+import pytest
+from v1.banks.serializers.bank import BankSerializer
+from thenewboston.serializers.primary_validator import PrimaryValidatorSerializer
+from thenewboston.utils.signed_requests import generate_signed_request
+from thenewboston.utils.format import format_address
+from thenewboston.accounts.manage import create_account
 
 
-def bank_config():
-    pass
+@pytest.fixture
+def primary_validator_config(primary_validator_configuration):
+    yield PrimaryValidatorSerializer(primary_validator_configuration).data
+
+
+@pytest.fixture
+def bank_config(bank, primary_validator_config):
+    bank_config = BankSerializer(bank).data
+    bank_config['node_type'] = 'BANK'
+    bank_config['primary_validator'] = primary_validator_config
+    yield bank_config
+
+
+@pytest.fixture
+def bank_connection_requests_signed_request(bank, signing_key):
+    yield generate_signed_request(
+        data={
+            "ip_address": bank.ip_address,
+            "port": bank.port,
+            "protocol": bank.protocol,
+        },
+        nid_signing_key=signing_key
+    )
+
+@pytest.fixture
+def bank_connection_requests_signed_request_new_node_identifier(bank):
+    signing_key, _ = create_account()
+    yield generate_signed_request(
+        data={
+            "ip_address": bank.ip_address,
+            "port": bank.port,
+            "protocol": bank.protocol,
+        },
+        nid_signing_key=signing_key
+    )
+
+
+@pytest.fixture
+def bank_address(bank):
+    yield format_address(
+        ip_address=bank.ip_address,
+        port=bank.port,
+        protocol=bank.protocol
+    )
--- v1/connection_requests/tests/connection_requests.py
+++ v1/connection_requests/tests/connection_requests.py
@@ -1,46 +1,57 @@
 import pytest
+from faker import Faker
 from thenewboston.utils.format import format_address
 from v1.banks.serializers.bank import BankSerializer
 from rest_framework.reverse import reverse
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from thenewboston.utils.signed_requests import generate_signed_request
 from v1.banks.models.bank import Bank
+from thenewboston.serializers.primary_validator import PrimaryValidatorSerializer
+from thenewboston.constants.network import VERIFY_KEY_LENGTH
+from thenewboston.verify_keys.verify_key import encode_verify_key
 
 
-@pytest.mark.parametrize(
-    'code, message',
-    [(HTTP_201_CREATED, ''), (HTTP_400_BAD_REQUEST, 'Already connected to bank')]
-)
-def test_validator_post_bank(client, bank, requests_mock, signing_key, code, message):
-    if code == HTTP_201_CREATED:
-        Bank.objects.all().delete()
+def test_validator_post_bank_success(
+    client, bank_config, requests_mock, bank_connection_requests_signed_request, bank_address
+):
+    Bank.objects.all().delete()
 
-    bank_address = format_address(
-        ip_address=bank.ip_address,
-        port=bank.port,
-        protocol=bank.protocol
-    )
-    bank_config_response = BankSerializer(bank).data
-    bank_config_response['node_type'] = 'BANK'
-    bank_config_response['primary_validator'] = {}
     requests_mock.get(
         f'{bank_address}/config',
-        json=bank_config_response,
+        json=bank_config,
     )
-    print(f'aaaaa {BankSerializer(bank).data}')
 
     response = client.post_json(
         reverse('connection_requests'),
-        generate_signed_request(
-            data={
-                "ip_address": bank.ip_address,
-                "port": bank.port,
-                "protocol": bank.protocol,
-            },
-            nid_signing_key=signing_key
-        ),
-        expected=code
+        bank_connection_requests_signed_request,
+        expected=HTTP_201_CREATED,
+    )
+    assert response == {}
+
+
+def test_validator_post_bank_existed_validate_node_identifier(
+    client, bank_connection_requests_signed_request
+):
+    response = client.post_json(
+        reverse('connection_requests'),
+        bank_connection_requests_signed_request,
+        expected=HTTP_400_BAD_REQUEST,
     )
 
-    if message:
-        assert message in response
+    assert response['node_identifier'] == ['Bank with that node identifier already exists']
+
+
+def test_validator_post_bank_already_connected(
+    client, bank_config, requests_mock, bank_connection_requests_signed_request_new_node_identifier, bank_address
+):
+    requests_mock.get(
+        f'{bank_address}/config',
+        json=bank_config,
+    )
+
+    response = client.post_json(
+        reverse('connection_requests'),
+        bank_connection_requests_signed_request_new_node_identifier,
+        expected=HTTP_400_BAD_REQUEST,
+    )
+    assert response['non_field_errors'] == ["Already connected to bank"]
